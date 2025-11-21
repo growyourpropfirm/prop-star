60d94937a45cb2cff1c9f725908209209a75f98e
“iobaidu5” <i.obaidu5@@gmail.com>
2025-11-22 01:56:00 +0500

    add favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
-import favicon from "../public/assets/icons/favicon.png"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,16 +7,15 @@ export const metadata = {
   title: 'Prop-Star | Trade Risk-Free. Prove Your Skills.',
   description:
     'Experience Prop-Star’s risk-free trader evaluation program with transparent rules, instant feedback, and fast funding opportunities.',
+  icons: {
+    icon: '/assets/icons/favicon.png',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-            <head>
-        <link rel="icon" href={favicon} />
-      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
